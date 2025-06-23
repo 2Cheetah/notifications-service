d3a30bff69525b3ac07f876aeaef89c2dc53daab
Denis Safiullin <fckninja@gmail.com>
2025-06-23 15:02:00 +0000

    refactor TestHttpEcho for clarity and consistency in assertions

--- main_test.go
+++ main_test.go
@@ -8,19 +8,39 @@ import (
 )
 
 func TestHttpEcho(t *testing.T) {
+	// Arrange
 	http.HandleFunc("POST /echo", EchoHandler)
 	rr := httptest.NewRecorder()
-	body := strings.NewReader("hello")
-	req, err := http.NewRequest(http.MethodPost, "/echo", body)
+	data := "hello"
+	reqBody := strings.NewReader(data)
+	req, err := http.NewRequest(http.MethodPost, "/echo", reqBody)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Act
 	http.DefaultServeMux.ServeHTTP(rr, req)
 
-	if rr.Code != http.StatusOK {
-		t.Errorf("Response code is not ok: %v", rr.Code)
+	// Assert
+	if rr.Body.String() != data {
+		t.Errorf("Wrong response body. Expected: %s, got: %s", data, rr.Body.String())
 	}
-	if expected, actual := "hello", rr.Body.String(); expected != actual {
-		t.Errorf("Wrong request body. Expected: %s, actual: %s", expected, actual)
+	if rr.Code != http.StatusOK {
+		t.Errorf("Wrong status code. Expected: %d, got: %d", http.StatusOK, rr.Code)
+	}
+
+	// Arrange
+	rr = httptest.NewRecorder()
+	req, err = http.NewRequest(http.MethodGet, "/echo", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Act
+	http.DefaultServeMux.ServeHTTP(rr, req)
+
+	// Assert
+	if rr.Code != http.StatusMethodNotAllowed {
+		t.Errorf("Wrong status code. Expected: %d, got: %d", http.StatusMethodNotAllowed, rr.Code)
 	}
 }
